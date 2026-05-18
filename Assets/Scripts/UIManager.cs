4c1bd43f02465af554500cc4769e38529ac8a802
Nicojerson <nicojersonsky1234@gmail.com>
2026-05-18 14:22:59 -0300

    Avances

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public GameObject goCartelPresione;
+    // Start is called before the first frame update
+    void Start()
+    {
+        goCartelPresione = GameObject.FindGameObjectWithTag("CartelPresione");
+        HideCartelpresione();
+    }
+
+    public void ShowCartelPresione()
+    {
+        goCartelPresione.SetActive(true);
+    }
+    public void HideCartelpresione()
+    {
+        goCartelPresione.SetActive(false);
+    }
+}
